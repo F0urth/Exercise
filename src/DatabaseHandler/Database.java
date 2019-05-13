f9a3f762e24e87a92477432714e6cde96a356c34
F0urth <f0urth.kamil@gmail.com>
2019-05-13 17:42:30 +0200

    Initialization project

---
+++ src/DatabaseHandler/Database.java
@@ -0,0 +1,13 @@
+package DatabaseHandler;
+
+public
+    class Database {
+
+    public static Database getInstance() {
+        return new Database();
+    }
+
+    private Database() {
+
+    }
+}
