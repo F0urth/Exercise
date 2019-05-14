0dee939a6b886b5408011165632ce5a505257260
F0urth <f0urth.kamil@gmail.com>
2019-05-14 23:09:44 +0200

    Closing Contact class

--- src/main/java/ReadData/ProcessContainers/Contact.java
+++ src/main/java/ReadData/ProcessContainers/Contact.java
@@ -7,15 +7,18 @@ import java.util.regex.Pattern;
 
 
 /**
+ * Class what represents Contact which gonna be translate into
+ * data tho database
+ *
  * @author F0urth
  */
 
-public
+public final
     class Contact {
 
     private static final String table_name;
-
     private static final Pattern emailPattern;
+
     private final Integer id;
     private final Integer id_customer;
     private final Integer type;
@@ -26,14 +29,24 @@ public
 
         /*
          * RFC 5322 Official Standard
+         * if we like to simplify this we can write simple \S+@\S+
+         * and for example send veryfication email
          */
-        emailPattern = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-" +
-            "\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\"" +
-            ")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]" +
-            "|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\" +
-            "x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
+        emailPattern = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:" +
+            "[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f]" +
+            ")*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]" +
+            "|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c" +
+            "\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
     }
 
+    /**
+     * Factory method
+     * @param id
+     * @param id_customer
+     * @param type
+     * @param contact
+     * @return new Contact class easy changable to sql query in String format
+     */
     public static Contact newInstance(Integer id, Integer id_customer, Integer type, String contact) {
         return new Contact(id, id_customer, type, contact);
     }
@@ -45,6 +58,10 @@ public
         this.contact = contact;
     }
 
+    /**
+     *
+     * @return SQL query in String format based of properties of this class
+     */
     String buildQuery() {
         return SQLBuilder.newInstance()
             .addSQLKeyword(SQLKeyword.INSERT)
@@ -54,6 +71,11 @@ public
             .buildQuery();
     }
 
+    /**
+     * Checking Type of the Contact
+     * @param contact
+     * @return Type of the Contact (integer - 0 - unknown, 1 - email, 2 - phone, 3- jabber)
+     */
     public static Integer getContactType(String contact) {
         String trim = contact.trim();
         if (trim.isEmpty())
@@ -67,7 +89,3 @@ public
 
     }
 }
-
-/*
- (?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])
- */
