38b3797e95f3c27416bb89292b39fbab9bc269b9
F0urth <f0urth.kamil@gmail.com>
2019-05-14 22:17:13 +0200

    Add Query building to the Containers classes

--- src/main/java/ReadData/ProcessContainers/Contact.java
+++ src/main/java/ReadData/ProcessContainers/Contact.java
@@ -1,16 +1,39 @@
 package ReadData.ProcessContainers;
 
+import DatabaseHandler.SQLBuilder.SQLBuilder;
+import DatabaseHandler.SQLBuilder.SQLKeyword;
+
 public
     class Contact {
 
+    private static final String table_name;
 
-    
+    private final Integer id;
+    private final Integer id_customer;
+    private final Integer type;
+    private final String contact;
 
-    public static Contact newInstance() {
-        return new Contact();
+    static {
+        table_name = "CONTACTS";
     }
 
-    private Contact() {
+    public static Contact newInstance(Integer id, Integer id_customer, Integer type, String contact) {
+        return new Contact(id, id_customer, type, contact);
+    }
 
+    private Contact(Integer id, Integer id_customer, Integer type, String contact) {
+        this.id = id;
+        this.id_customer = id_customer;
+        this.type = type;
+        this.contact = contact;
+    }
+
+    String buildQuery() {
+        return SQLBuilder.newInstance()
+            .addSQLKeyword(SQLKeyword.INSERT)
+            .setTable(table_name)
+            .addSQLKeyword(SQLKeyword.VALUES)
+            .setValues(id, id_customer, type, contact)
+            .buildQuery();
     }
 }
--- src/main/java/ReadData/ProcessContainers/Customer.java
+++ src/main/java/ReadData/ProcessContainers/Customer.java
@@ -1,20 +1,39 @@
 package ReadData.ProcessContainers;
 
+import DatabaseHandler.SQLBuilder.SQLBuilder;
+import DatabaseHandler.SQLBuilder.SQLKeyword;
+
 public
     class Customer {
+
+    private static final String table_name;
 
     private Integer id;
     private String name;
     private String surname;
     private Integer age;
 
-
-
-    public static Customer newInstance() {
-        return new Customer();
+    static {
+        table_name = "CUSTOMERS";
     }
 
-    private Customer() {
+    public static Customer newInstance(Integer id, String name, String surname, Integer age) {
+        return new Customer(id, name, surname, age);
+    }
 
+    private Customer(Integer id, String name, String surname, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    String buildSQLQuery() {
+        return SQLBuilder.newInstance()
+            .addSQLKeyword(SQLKeyword.INSERT)
+            .setTable(table_name)
+            .addSQLKeyword(SQLKeyword.VALUES)
+            .setValues(id, name, surname, age)
+            .buildQuery();
     }
 }
